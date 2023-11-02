eb510c5e358dbfe656094b82b740766f968f2997
Hayk Hovhannisyan <haik.hovhanisyan@gmail.com>
2023-11-03 03:50:23 +0400

    Add controller test

--- src/test/java/com/hhovhann/rsstrackerservice/feed/controller/FeedEntityControllerTest.java
+++ src/test/java/com/hhovhann/rsstrackerservice/feed/controller/FeedEntityControllerTest.java
@@ -1,10 +1,45 @@
 package com.hhovhann.rsstrackerservice.feed.controller;
 
+import com.hhovhann.rsstrackerservice.feed.dto.RequestFeedDto;
+import com.hhovhann.rsstrackerservice.feed.dto.ResponseFeedDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = "scheduler.enabled=false")
 class FeedEntityControllerTest {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void getFeedsByFilters() throws Exception {
+        var requestUrl = "http://localhost:" + port + "/" + "/tracker/api/v1/feeds/search";
+        assertThat(this.restTemplate.getForEntity(
+                requestUrl,
+                ResponseFeedDto[].class,
+                Map.of("categories", List.of(), "dateFrom", ZonedDateTime.now(), "dateTo", ZonedDateTime.now().plusDays(10))));
+    }
+
+    @Test
+    void searchFeeds() throws Exception {
+        var requestUrl = "http://localhost:" + port + "/" + "/tracker/api/v1/feeds/search";
+        assertThat(this.restTemplate.postForEntity(
+                requestUrl,
+                new RequestFeedDto(List.of(), ZonedDateTime.now(), ZonedDateTime.now().plusDays(10)),
+                ResponseFeedDto[].class));
+    }
 }
