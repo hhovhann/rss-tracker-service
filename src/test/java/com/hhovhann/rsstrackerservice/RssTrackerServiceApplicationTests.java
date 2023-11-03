03737b7fcc12251a54e037d6071f5520c6daba08
Hayk Hovhannisyan <haik.hovhanisyan@gmail.com>
2023-11-04 03:53:04 +0400

    add smoke test

--- src/test/java/com/hhovhann/rsstrackerservice/RssTrackerServiceApplicationTests.java
+++ src/test/java/com/hhovhann/rsstrackerservice/RssTrackerServiceApplicationTests.java
@@ -1,15 +1,22 @@
 package com.hhovhann.rsstrackerservice;
 
+import com.hhovhann.rsstrackerservice.feed.controller.FeedEntityController;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Ignore
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class RssTrackerServiceApplicationTests {
 
+    @Autowired
+    private FeedEntityController controller;
+
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
-
 }
