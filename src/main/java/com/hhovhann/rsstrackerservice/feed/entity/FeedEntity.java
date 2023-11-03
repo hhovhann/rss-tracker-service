a6e5a3a0c539612c103b78c662f9a822acad3d92
Hayk Hovhannisyan <haik.hovhanisyan@gmail.com>
2023-11-04 03:22:37 +0400

    update init scripts

--- src/main/java/com/hhovhann/rsstrackerservice/feed/entity/FeedEntity.java
+++ src/main/java/com/hhovhann/rsstrackerservice/feed/entity/FeedEntity.java
@@ -3,6 +3,7 @@ package com.hhovhann.rsstrackerservice.feed.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,7 +30,7 @@ public class FeedEntity {
     ZonedDateTime publicationDate;
     String author;
     String thumbnails;
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     List<String> categories = new ArrayList<>();
     String relatedIdentifiers;
 }
